78122f3bd2a87c8f320baf55664b8b378a193705
mohammed <mohammed.az2002@gmail.com>
2023-04-01 15:57:06 +0300

    update in multiplayer

--- Assets/scripts/CarHealth.cs
+++ Assets/scripts/CarHealth.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Photon.Pun;
 
 public class CarHealth : MonoBehaviour
 {
@@ -18,7 +19,62 @@ public class CarHealth : MonoBehaviour
 
         if (currentHealth <= 0)
         {
-            Destroy(gameObject);
+            PhotonView pv = GetComponent<PhotonView>();
+            pv.RPC("DestroyCar", RpcTarget.All);
         }
     }
+
+    [PunRPC]
+    void DestroyCar()
+    {
+        PhotonView pv = GetComponent<PhotonView>();
+        DestroyCar(pv.gameObject);
+        Destroy(gameObject);
+        //if (GetComponent<PhotonView>().IsMine)
+        //{
+        //    Renderer renderer = GetComponent<Renderer>();
+        //    if (renderer != null)
+        //    {
+        //        Destroy(renderer.gameObject);
+        //        //renderer.enabled = false;
+
+        //    }
+
+        //    Collider collider = GetComponent<Collider>();
+        //    if (collider != null)
+        //    {
+        //        collider.enabled = false;
+        //    }
+        //}
+
+       
+    }
 }
+
+
+
+
+//using UnityEngine;
+
+//public class CarHealth : MonoBehaviour
+//{
+//    public int maxHealth = 5;
+//    public int currentHealth;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        currentHealth = maxHealth;
+//    }
+
+//    public void TakeDamage(int damage)
+//    {
+//        currentHealth -= damage;
+//        Debug.Log("Car hit! Health: " + currentHealth);
+
+//        if (currentHealth <= 0)
+//        {
+//            Destroy(gameObject);
+//        }
+//    }
+//}
