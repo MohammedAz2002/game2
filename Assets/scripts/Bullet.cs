19333b29b45d16e9dba55f47e38a90db47905305
MohammedAz2002 <109413767+MohammedAz2002@users.noreply.github.com>
2023-12-06 14:11:05 +0200

    add animation explosion for the car

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public GameObject explosion1;
+    private GameObject explosion;
 
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        Invoke("InstantiateExplosion", 0.2f);
+
+
+    }
+    private void InstantiateExplosion()
+    {
+        explosion = Instantiate(explosion1, transform.position, Quaternion.identity);
+
+        // Destroy the gameObject after 1 second
+        if (gameObject != null)
+        {
+            Destroy(gameObject, 1f);
+        }
+
+        // Destroy the explosion after 1 second
+        Destroy(explosion, 1f);
+    }
 }
